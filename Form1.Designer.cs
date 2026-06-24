1b6e90d40771ee4a58681fb4ce201c39f1ae04ea
AxelDeJesuuHernandezCoronado <xlhernandezcoronado@gmail.com>
2026-06-24 15:22:54 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TaquillaCine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbTipoBoleto = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(112, 148);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(76, 13);
+            this.lblTipo.TabIndex = 1;
+            this.lblTipo.Text = "Tipo de Boleto";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(112, 358);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(73, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Total a pagar:";
+            // 
+            // cmbTipoBoleto
+            // 
+            this.cmbTipoBoleto.FormattingEnabled = true;
+            this.cmbTipoBoleto.Items.AddRange(new object[] {
+            "Estudiante",
+            "Adulto Mayor",
+            "General"});
+            this.cmbTipoBoleto.Location = new System.Drawing.Point(115, 208);
+            this.cmbTipoBoleto.Name = "cmbTipoBoleto";
+            this.cmbTipoBoleto.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoBoleto.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(115, 278);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(112, 58);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(127, 13);
+            this.lblTitulo.TabIndex = 5;
+            this.lblTitulo.Text = "Venta de Boletos de Cine";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbTipoBoleto);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTipo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbTipoBoleto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaquillaCine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Boleto boleto = null;
+
+            string tipo = cmbTipoBoleto.SelectedItem.ToString();
+
+            if (tipo == "Estudiante")
+            {
+
+            }
+
+            else if (tipo == "Adulto Mayor")
+            {
+
+            }
+
+            else if (tipo == "General")
+            {
+
+            }
+
+            if (boleto != null)
+            {
+                lblResultado.Text = "Total a pagar: $" + boleto.CalcularPrecioFinal();
+            }
+        }
+    }
+}
