c4784784bd53e8cc68c8d44a96f96131c9b6d2f3
Yesenia Magdalena <yesenia21th55@gmail.com>
2026-06-24 18:03:52 -0600

    Clase BoletoEstudiante terminada con atributo Matricula y descuento del 30%

---
+++ BoletoEstudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaquillaCine
+{
+    public class BoletoEstudiante : Boleto
+    {
+        public string Matricula;
+        public BoletoEstudiante(decimal precioBase) : base(precioBase)
+        {
+        }
+
+        public override decimal CalcularPrecioFinal()
+        {
+            decimal Descuento = PrecioBase * 30 / 100;
+            return PrecioBase - Descuento;
+        }
+        
+    }
+}
